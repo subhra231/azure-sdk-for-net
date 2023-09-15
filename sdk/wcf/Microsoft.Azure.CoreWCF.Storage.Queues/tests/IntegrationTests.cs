28b9c547e08d4df23a06ad36bf0d447b24d3a54a
subhra231 <subhra.nits@gmail.com>
2023-09-14 21:58:25 -0700

    update tests to use azurite

--- sdk/wcf/Microsoft.Azure.CoreWCF.Storage.Queues/tests/IntegrationTests.cs
+++ sdk/wcf/Microsoft.Azure.CoreWCF.Storage.Queues/tests/IntegrationTests.cs
@@ -1,13 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Azure.Storage.Queues;
 using Contracts;
 using CoreWCF.AzureQueueStorage.Tests.Helpers;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using System.Net.Http;
 
 namespace CoreWCF.AzureQueueStorage.Tests
 {
@@ -15,20 +15,17 @@ namespace CoreWCF.AzureQueueStorage.Tests
     public class IntegrationTests
     {
         private IWebHost host;
-        private HttpClient httpClient;
 
         [SetUp]
         public void Setup()
         {
             host = ServiceHelper.CreateWebHostBuilder<Startup>().Build();
             host.Start();
-            httpClient = new HttpClient();
         }
 
         [TearDown]
         public void TearDown()
         {
-            httpClient.Dispose();
             host.StopAsync().Wait();
             host.Dispose();
         }
@@ -36,8 +33,8 @@ namespace CoreWCF.AzureQueueStorage.Tests
         [Test]
         public async Task DefaultQueueConfiguration_ReceiveMessage_Success()
         {
-            var queue = host.Services.GetRequiredService<DisposingQueue>();
-            await queue.Queue.SendMessageAsync("");
+            var queue = host.Services.GetRequiredService<QueueClient>();
+            await queue.SendMessageAsync("<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:a=\"http://www.w3.org/2005/08/addressing\"><s:Header><a:Action s:mustUnderstand=\"1\">http://tempuri.org/ITestContract/Create</a:Action></s:Header><s:Body><Create xmlns=\"http://tempuri.org/\"><name>test</name></Create></s:Body></s:Envelope>");
 
             var testService = host.Services.GetRequiredService<TestService>();
             Assert.True(testService.ManualResetEvent.Wait(System.TimeSpan.FromSeconds(5)));
--- sdk/wcf/Microsoft.Azure.CoreWCF.Storage.Queues/tests/Startup.cs
+++ sdk/wcf/Microsoft.Azure.CoreWCF.Storage.Queues/tests/Startup.cs
@@ -1,48 +1,38 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Threading.Tasks;
+using Azure.Storage.CoreWCF.Channels;
+using Azure.Storage.Queues;
 using Contracts;
 using CoreWCF.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Azure.Storage.CoreWCF.Channels;
-using QueuesClientBuilder = Azure.Storage.Test.Shared.ClientBuilder<
-        Azure.Storage.Queues.QueueServiceClient,
-        Azure.Storage.Queues.QueueClientOptions>;
 
 namespace CoreWCF.AzureQueueStorage.Tests
 {
     public class Startup
     {
+        private readonly string queueName = "queue-name";
+        private readonly string connectionString = "DefaultEndpointsProtocol=https;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=https://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=https://127.0.0.1:10001/devstoreaccount1;";
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<TestService>();
             services.AddServiceModelServices();
 
-            //add the disposable queue to DI
-            services.AddSingleton(provider =>
-            {
-                QueueTestBase queueTestBase = new QueueTestBase(true);
-                Task<DisposingQueue> task = queueTestBase.GetTestQueueAsync();
-                task.Wait();
-                return task.Result;
-            });
+            var queueClient = new QueueClient(connectionString, queueName);
+            queueClient.Create();
+            services.AddSingleton(queueClient);
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            DisposingQueue disposingQueue = app.ApplicationServices.GetRequiredService<DisposingQueue>();
-            QueuesClientBuilder clientBuilder = default;
-            string accountName = clientBuilder.Tenants.TestConfigDefault.AccountName;
-            string accountKey = clientBuilder.Tenants.TestConfigDefault.AccountKey;
-            string connectionString = $"DefaultEndpointsProtocol=https;AccountName={accountName};AccountKey={accountKey}";
+            QueueClient queue = app.ApplicationServices.GetRequiredService<QueueClient>();
 
             app.UseServiceModel(services =>
             {
                 services.AddService<TestService>();
-                services.AddServiceEndpoint<TestService, ITestContract>(new AzureQueueStorageBinding(connectionString, disposingQueue.Queue.Name),
-                   AzureQueueStorageQueueNameConverter.GetEndpointUrl(disposingQueue.Queue.AccountName, disposingQueue.Queue.Name));
+                services.AddServiceEndpoint<TestService, ITestContract>(new AzureQueueStorageBinding(connectionString, queueName),
+                   AzureQueueStorageQueueNameConverter.GetEndpointUrl("devstoreaccount1" , queueName));
             });
         }
     }
